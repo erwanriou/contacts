b95551df8f92dc7c4cd09a416248c764584fbc26
Erwan Riou <riou@viamundus.com>
2018-04-30 17:28:34 +0200

    Contact API implemented, starting the react router dom

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContact';
+import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state = {
-    contacts: [
-     {
-       "id": "karen",
-       "name": "Karen Isgrigg",
-       "handle": "karen_isgrigg",
-       "avatarURL": "http://localhost:5001/karen.jpg"
-     },
-     {
-       "id": "richard",
-       "name": "Richard Kalehoff",
-       "handle": "richardkalehoff",
-       "avatarURL": "http://localhost:5001/richard.jpg"
-     },
-     {
-       "id": "tyler",
-       "name": "Tyler McGinnis",
-       "handle": "tylermcginnis",
-       "avatarURL": "http://localhost:5001/tyler.jpg"
-     }
-    ]
+    contacts: [],
+    screen: 'list'
+  }
+
+  componentDidMount() {
+    ContactsAPI.getAll()
+      .then((contacts) => {
+        this.setState(() => ({
+          contacts
+        }))
+      })
   }
 
   removeContact = (contact) => {
@@ -32,16 +24,26 @@ class App extends Component {
         return c.id !== contact.id;
       })
     }))
+    ContactsAPI.remove(contact)
   }
 
   render() {
     return (
       <div>
-        <ListContacts
+        {this.state.screen === 'list' && (<ListContacts
           contacts={this.state.contacts}
           onDeleteContact={this.removeContact}
+          onNavigate={() => {
+            this.setState(() => ({
+              screen: 'create'
+            }))
+          }}
         />
+        )}
+        {this.state.screen === 'create' && (<CreateContact />
+        )}
       </div>
+
     );
   }
 }
